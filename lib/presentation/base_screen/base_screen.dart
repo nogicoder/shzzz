7888196df513541bafb61f41deb11bd02969fcf3
Mai Bach <bachnv@sunshinegroup.vn>
2022-01-14 17:52:43 +0700

    Update main screen

--- lib/presentation/base_screen/base_screen.dart
+++ lib/presentation/base_screen/base_screen.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:shzzz/business/services/index.dart';
 import 'package:shzzz/shared/constants.dart';
+import 'package:shzzz/shared/index.dart';
 
 class BaseScreen extends StatelessWidget {
   const BaseScreen({Key? key}) : super(key: key);
@@ -9,20 +13,82 @@ class BaseScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async => false,
-      child: Material(
-        child: SafeArea(
-          child: Stack(
-            children: [
-              Positioned(
-                left: Constants.kOuterPadding,
-                child: Text(
-                  'Hi ${userConfigService.userInfo?.name ?? ''}, wish you a nice day!',
-                ),
-              ),
-            ],
-          ),
+      child: CupertinoTabScaffold(
+        tabBar: CupertinoTabBar(
+          backgroundColor: Get.theme.primaryColor,
+          activeColor: Get.theme.colorScheme.surface,
+          iconSize: 24,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.check_mark_circled),
+              label: 'Ongoing',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.clear_circled),
+              label: 'Completed',
+            ),
+          ],
         ),
+        tabBuilder: (context, index) {
+          switch (index) {
+            case 0:
+            default:
+              return _buildFirstTab();
+          }
+        },
       ),
     );
+  }
+
+  Widget _buildFirstTab() {
+    return Container(
+      child: Stack(
+        children: [
+          Positioned(
+            top: 120,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(25),
+                  topRight: Radius.circular(25),
+                ),
+              ),
+              child: CustomScrollView(
+                slivers: [
+                  SliverPadding(
+                    padding: Constants.kHorizontalPaddingStandard,
+                    sliver: SliverToBoxAdapter(
+                      child: Text(
+                        "What's up, ${userConfigService.userInfo?.name ?? ''}!",
+                        style: UITextStyle.headline4(FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  SliverList(
+                    delegate: SliverChildListDelegate(
+                      List<Widget>.generate(
+                        10,
+                        (index) => _buildItem(index),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildItem(int index) {
+    return Container();
   }
 }
--- lib/shared/theme/ui_theme.dart
+++ lib/shared/theme/ui_theme.dart
@@ -4,18 +4,22 @@ class UITheme {
   static ThemeData getLightTheme() {
     final baseTheme = ThemeData.light();
     return baseTheme.copyWith(
-        primaryColor: const Color(0xFF6867AC),
-        colorScheme: baseTheme.colorScheme.copyWith(
-          secondary: const Color(0xFFA267AC),
+      // primaryColor: const Color(0xFF020417),
+      // backgroundColor: const Color(0xFF020417),
+      // colorScheme: baseTheme.colorScheme.copyWith(
+      //   secondary: const Color(0xFFA267AC),
+      //   surface: const Color(0xFFCE7BB0),
+      //   background: const Color(0xFFFFBCD1),
+      // ),
+      textTheme: baseTheme.textTheme.copyWith(
+        bodyText2: baseTheme.textTheme.bodyText2?.copyWith(
+          color: const Color(0xFF020417),
+          fontSize: 14,
         ),
-        textTheme: baseTheme.textTheme.copyWith(
-          bodyText2: baseTheme.textTheme.bodyText2?.copyWith(
-            color: const Color(0xFFFFBCD1),
-            fontSize: 14,
-          ),
-          headline5: baseTheme.textTheme.headline5
-              ?.copyWith(color: const Color(0xFFFFBCD1)),
-        ));
+        headline4: baseTheme.textTheme.headline6
+            ?.copyWith(color: const Color(0xFF373b5e)),
+      ),
+    );
   }
 
   static ThemeData getDarkTheme() {
